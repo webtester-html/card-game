3e23d18e4546a36debf834147cb00b7a91b08651
webtester-html <redmikomputer@gmail.com>
2025-05-11 21:44:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -229,27 +229,26 @@ app.use(express.static(__dirname, {
     }
 }));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/room', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'room.html'));
+});
+
+app.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'game.html'));
+});
+
 app.get('/style.css', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
-    res.sendFile(path.join(__dirname, 'style.css'), (err) => {
+    res.sendFile(path.join(__dirname, 'public', 'style.css'), (err) => {
         if (err) {
             console.error('Error serving style.css:', err.message);
             res.status(404).send('CSS file not found');
         }
     });
-});
-
-// Routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/room', (req, res) => {
-    res.sendFile(path.join(__dirname, 'room.html'));
-});
-
-app.get('/game', (req, res) => {
-    res.sendFile(path.join(__dirname, 'game.html'));
 });
 
 // Endpoint to fetch room state
